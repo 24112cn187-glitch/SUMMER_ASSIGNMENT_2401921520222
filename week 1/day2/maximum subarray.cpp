ae38b5d6ecf9f8e7d9a981696542d78da8f47a6c
sambhav goel <24112cn187@glbitm.ac.in>
2026-06-02 21:51:33 +0530

    Create maximum subarray.cpp

---
+++ week 1/day2/maximum subarray.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+
+        int n= nums.size();
+        int sum=0;
+        int maxsum=INT_MIN;
+
+        for(int i=0;i<n;i++)
+        {
+            if(sum<0)
+            {
+                sum=0;
+            }
+            sum = sum+nums[i];
+            maxsum=max(maxsum,sum);
+            
+        }
+
+        return maxsum;
+        
+    }
+};
