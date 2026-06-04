630badb197236f096460f2020a35c474a496ad06
sambhav goel <24112cn187@glbitm.ac.in>
2026-06-04 22:49:30 +0530

    Create matrixdiagnol.cpp

---
+++ week 1/day4/matrixdiagnol.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int diagonalSum(vector<vector<int>>& mat) {
+
+        int n=mat.size();
+        int sum =0;
+
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(i==j)
+                {
+                    sum= sum+mat[i][j];
+                }
+
+                else if(i+j+1==n)
+                {
+                    if(i==j) continue;
+                    sum=sum+mat[i][j];
+                }
+            }
+        }
+
+        return sum;
+        
+    }
+};
