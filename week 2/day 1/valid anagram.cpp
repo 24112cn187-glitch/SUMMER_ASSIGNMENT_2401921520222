a0d5eea6d0c34aeb77068fdf5d9ad5372188874e
sambhav goel <24112cn187@glbitm.ac.in>
2026-06-08 23:17:54 +0530

    Create valid anagram.cpp

---
+++ week 2/day 1/valid anagram.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+
+
+         int n1=s.size();
+         int n2=t.size();
+
+         sort(s.begin(),s.end());
+         sort(t.begin(),t.end());
+
+         if(s==t) return true;
+         else return false;
+    }
+};
