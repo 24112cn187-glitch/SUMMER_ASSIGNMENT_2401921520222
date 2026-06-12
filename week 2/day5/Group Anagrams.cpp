a319c1efd00a66c0ecd5e26c8b2fa1cc3a35b039
sambhav goel <24112cn187@glbitm.ac.in>
2026-06-12 17:13:44 +0530

    Create Group Anagrams.cpp

---
+++ week 2/day5/Group Anagrams.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+
+        int n=strs.size();
+
+        unordered_map<string,vector<string>>m;
+
+        for(int i=0;i<n;i++)
+        {
+            string temp=strs[i];
+            sort(temp.begin(),temp.end());
+
+            m[temp].push_back(strs[i]);
+        }
+
+         vector<vector<string>>ans;
+
+        for(auto el:m)
+        {
+            ans.push_back(el.second);
+        }
+        return ans;
+        
+    }
+};
