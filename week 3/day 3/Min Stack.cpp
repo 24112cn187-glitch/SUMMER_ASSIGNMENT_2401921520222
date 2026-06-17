9560511b855bb38eca66689b1f37e36d3b0ff5ec
sambhav goel <24112cn187@glbitm.ac.in>
2026-06-18 00:07:03 +0530

    Create Min Stack.cpp

---
+++ week 3/day 3/Min Stack.cpp
@@ -0,0 +1,66 @@
+class MinStack {
+public:
+
+    
+    stack<long long> st;
+    long long min;
+
+    MinStack() {
+        min=INT_MAX;
+        
+    }
+
+    
+    
+    void push(int val) {
+
+        long long x= (long long)val;
+
+        if(st.size()==0)
+        {
+            st.push(x);
+            min=x;
+        }
+
+        else if (x>=min) st.push(val);
+
+        else 
+        {
+            st.push(2*x-min);
+            min=x;
+
+        }
+       
+
+
+
+        
+    }
+    
+    void pop() {
+
+        if(st.top()<min)
+        {
+            long long oldmin=2*min-st.top();
+            min=oldmin;
+        }
+
+        st.pop();
+       
+    }
+    
+    int top() {
+
+        if(st.top()<min) return min;
+        else return st.top();
+       
+        
+    }
+    
+    int getMin() {
+
+        return min;
+        
+        
+    }
+};
