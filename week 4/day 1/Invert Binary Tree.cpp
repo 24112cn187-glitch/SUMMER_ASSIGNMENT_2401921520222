eb928f32e52c4b1731f8c5a6d2683a93c2d9f72b
sambhav goel <24112cn187@glbitm.ac.in>
2026-06-23 02:40:45 +0530

    Create Invert Binary Tree.cpp

---
+++ week 4/day 1/Invert Binary Tree.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+
+        if(root==NULL) return root;
+
+        TreeNode* l=root->left;
+        TreeNode* r=root->right;
+
+        root->left=r;
+        root->right=l;
+
+        invertTree(root->left);
+        invertTree(root->right);
+
+        return root;
+        
+    }
+};
